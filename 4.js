7d809a02bdd86647f27107a50c7a902db3a52f35
ArtemPro89 <prosvirnikov1989@gmail.com>
2022-07-15 20:42:06 +0300

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,7 @@
+var footballClubs = [
+    {name: "Leeds United", kitColor: "white", sdadium: "Elland road", bestPlayers: [10, 15, 45]},
+    {name: "Manchester United", kitColor: "red", sdadium: "Old Trafford", bestPlayers: [4, 8, 14]},
+    {name: "Liverpool", kitColor: "red", stadium: "Anfild Road", bestPlayers: [47, 15, 5, 6]}
+];
+footballClubs[2].bestPlayers[3];
+6
